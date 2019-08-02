f1f63e02fb59fe33f58aa78ffe529662c47b1867
nskazki <nskazki@gmail.com>
2019-08-03 02:46:00 +0300

    Build lib

---
+++ dist/intabbr.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = intabbr;
+
+function intabbr(num, _ref) {
+  var abbreviations = _ref.abbreviations;
+  var abs = Math.abs(num);
+  var val = num;
+  var suf = '';
+
+  if (abs >= Math.pow(10, 12)) {
+    // trillion
+    suf = clearSuffix(abbreviations.trillion);
+    val = Math.round(val / Math.pow(10, 12));
+  } else if (abs < Math.pow(10, 12) && abs >= Math.pow(10, 9)) {
+    // billion
+    suf = clearSuffix(abbreviations.billion);
+    val = Math.round(val / Math.pow(10, 9));
+  } else if (abs < Math.pow(10, 9) && abs >= Math.pow(10, 6)) {
+    // million
+    suf = clearSuffix(abbreviations.million);
+    val = Math.round(val / Math.pow(10, 6));
+  } else if (abs < Math.pow(10, 6) && abs >= Math.pow(10, 3)) {
+    // thousand
+    suf = clearSuffix(abbreviations.thousand);
+    val = Math.round(val / Math.pow(10, 3));
+  }
+
+  if (!suf) {
+    return val;
+  } else if (suf.length === 1) {
+    return "".concat(val).concat(toUpper(suf));
+  } else {
+    return "".concat(val, " ").concat(upperFirst(suf));
+  }
+}
+
+function toUpper(str) {
+  return str.toUpperCase();
+}
+
+function upperFirst(str) {
+  return toUpper(str[0]) + str.slice(1);
+}
+
+function clearSuffix(suf) {
+  return suf.replace(/\./g, '').trim();
+}
---
+++ dist/intcomma.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = intcomma;
+
+function intcomma(input, _ref) {
+  var delimiters = _ref.delimiters;
+  var decimalSeparator = delimiters.decimal;
+  var thousandSeparator = delimiters.thousands;
+  var thousandsSize = delimiters.thousandsSize || 3;
+  var characteristic = input.toString().split('.')[0];
+  var mantissa = input.toString().split('.')[1];
+  var hasNegativeSign = input < 0 && characteristic.indexOf('-') === 0;
+
+  if (hasNegativeSign) {
+    // Remove the negative sign
+    characteristic = characteristic.slice(1);
+  }
+
+  var indexesToInsertThousandDelimiters = indexesOfGroupSpaces(characteristic.length, thousandsSize);
+  indexesToInsertThousandDelimiters.forEach(function (position, index) {
+    characteristic = characteristic.slice(0, position + index) + thousandSeparator + characteristic.slice(position + index);
+  });
+
+  if (hasNegativeSign) {
+    // Add back the negative sign
+    characteristic = "-".concat(characteristic);
+  }
+
+  if (!mantissa) {
+    return characteristic;
+  } else {
+    return characteristic + decimalSeparator + mantissa;
+  }
+}
+
+function indexesOfGroupSpaces(totalLength, groupSize) {
+  var result = [];
+  var counter = 0;
+
+  for (var i = totalLength; i > 0; i--) {
+    if (counter === groupSize) {
+      result.unshift(i);
+      counter = 0;
+    }
+
+    counter++;
+  }
+
+  return result;
+}
